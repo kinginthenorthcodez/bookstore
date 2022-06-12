0bc621c0a4011f744c4e9814f21302c6b42e462f
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-06-12 05:32:35 +0530

    Fix linter

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,30 +2,32 @@
 
 const initialState = [
   {
-    id: '1',
-    title: 'Expanse',
-    author: 'Maqueen',
-    category: 'Sci-fi',
+    id: "1",
+    title: "Expanse",
+    author: "Maqueen",
+    category: "Sci-fi",
   },
   {
-    id: '2',
-    title: 'The Wheel of Time',
-    author: 'J.R.Rowlings',
-    category: 'Epic',
+    id: "2",
+    title: "The Wheel of Time",
+    author: "J.R.Rowlings",
+    category: "Epic",
   },
   {
-    id: '3',
-    title: 'Game of thrones',
-    author: 'George.R.Martin',
-    category: 'Epic',
+    id: "3",
+    title: "Game of thrones",
+    author: "George.R.Martin",
+    category: "Epic",
   },
 ];
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_BOOK':
+    case "ADD_BOOK":
       return [...state, action.payload];
-    case 'REMOVE_BOOK':
+    case "REMOVE_BOOK":
       return state.filter((book) => book.id !== action.payload);
+    case "FETCH_OK":
+      return "OK";
     default:
       return state;
   }
@@ -34,12 +36,12 @@ const reducer = (state = initialState, action) => {
 // actions
 
 export const addBook = (book) => ({
-  type: 'ADD_BOOK',
+  type: "ADD_BOOK",
   payload: book,
 });
 
 export const removeBook = (id) => ({
-  type: 'REMOVE_BOOK',
+  type: "REMOVE_BOOK",
   payload: id,
 });
 
